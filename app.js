a472bcf3174ed281a9b754b3291a386c81e4b0d0
MMKaragoz <mmertkaragoz@gmail.com>
2021-05-26 16:05:44 +0300

    hw8

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    name: 'Blog Name',
-    description: 'Blog Description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const PORT = 3000;
